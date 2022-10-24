ed32411523b155ff3e46744a0923e3e32a0fb89e
limsaehyun <sh00710000@gmail.com>
2022-10-24 19:14:10 +0900

    init: (#1) init project

---
+++ src/test/kotlin/com/saehyun/smonkey/SMonkeyApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.saehyun.smonkey
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class SMonkeyApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
