3a264b27db1872d485bb8388b966732d259dc9fa
limsaehyun <sh00710000@gmail.com>
2022-10-26 16:13:54 +0900

    feat: (#3) JwtProperties

---
+++ src/main/kotlin/com/saehyun/smonkey/global/security/properties/JwtProperties.kt
@@ -0,0 +1,29 @@
+package com.saehyun.smonkey.global.security.properties
+
+import org.springframework.boot.context.properties.ConfigurationProperties
+import org.springframework.boot.context.properties.ConstructorBinding
+
+/**
+ * JWT의 속성을 가지고 있는 [JwtProperties]
+ *
+ * @param secretKey secretKey of jtw
+ * @param accessTokenExp accessToken Expired time
+ * @param refreshTokenExp refreshToken Expired time
+ */
+
+// TODO ("Issue! 어노테이션 오류가 발생함")
+//@ConfigurationProperties(prefix = "jwt")
+@ConstructorBinding
+class JwtProperties(
+    val secretKey: String,
+    val accessTokenExp: Long,
+    val refreshTokenExp: Long
+) {
+
+    companion object {
+        const val TOKEN_PREFIX = "Bearer "
+        const val TOKEN_HEADER_NAME = "Authorization"
+        const val ACCESS_VALUE = "access"
+        const val REFRESH_VALUE = "refresh"
+    }
+}
