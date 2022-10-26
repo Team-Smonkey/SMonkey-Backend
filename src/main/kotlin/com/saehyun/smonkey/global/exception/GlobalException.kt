021317c8329d87248c64dde63c95c4787a48e8dc
limsaehyun <sh00710000@gmail.com>
2022-10-26 16:14:10 +0900

    feat: (#3) GlobalException

---
+++ src/main/kotlin/com/saehyun/smonkey/global/exception/GlobalException.kt
@@ -0,0 +1,11 @@
+package com.saehyun.smonkey.global.exception
+
+import com.saehyun.smonkey.global.exception.error.ErrorResponse
+
+open class GlobalException(private val error: ErrorResponse): RuntimeException() {
+    val status: Int
+    get() = error.status
+
+    override val message: String
+    get() = error.message
+}
