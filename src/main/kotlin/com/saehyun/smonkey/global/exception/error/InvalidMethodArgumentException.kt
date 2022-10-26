0a3d93c55c901bc2a37e70735cae37b0f7da5e74
limsaehyun <sh00710000@gmail.com>
2022-10-26 16:15:33 +0900

    init: (#3) InvalidMethodArgumentException

---
+++ src/main/kotlin/com/saehyun/smonkey/global/exception/error/InvalidMethodArgumentException.kt
@@ -0,0 +1,7 @@
+package com.saehyun.smonkey.global.exception.error
+
+import com.saehyun.smonkey.global.exception.GlobalException
+
+object InvalidMethodArgumentException: GlobalException(GlobalExceptionErrorCode.INVALID_METHOD_ARGUMENT) {
+    val EXCEPTION = InvalidMethodArgumentException
+}
