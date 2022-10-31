745954cd79b08f65b468f299d0afa47a044cea6b
limsaehyun <sh00710000@gmail.com>
2022-10-31 19:38:43 +0900

    feat: (#5) AuthDetailsService

--- src/main/kotlin/com/saehyun/smonkey/global/security/auth/AuthDetailsService.kt
+++ src/main/kotlin/com/saehyun/smonkey/global/security/auth/AuthDetailsService.kt
@@ -1,8 +1,19 @@
 package com.saehyun.smonkey.global.security.auth
 
+import com.saehyun.smonkey.domain.user.facade.UserFacade
+import org.springframework.security.core.userdetails.UserDetails
+import org.springframework.security.core.userdetails.UserDetailsService
 import org.springframework.stereotype.Service
 
 @Service
-class AuthDetailsService {
-    // TODO
+class AuthDetailsService(
+    private val userFacade: UserFacade,
+): UserDetailsService {
+    override fun loadUserByUsername(username: String): UserDetails {
+        val user = userFacade.getByAccountId(username)
+
+        return AuthDetails(
+            user = user,
+        )
+    }
 }
