21c1d0c3cf8f1d9d6f5c5c0ad5e7248e94f64caf
limsaehyun <sh00710000@gmail.com>
2022-11-02 00:21:41 +0900

    docs: (#7) 빠진 kDoc 추가

--- src/main/kotlin/com/saehyun/smonkey/domain/user/exception/UserNotFoundException.kt
+++ src/main/kotlin/com/saehyun/smonkey/domain/user/exception/UserNotFoundException.kt
@@ -3,6 +3,11 @@ package com.saehyun.smonkey.domain.user.exception
 import com.saehyun.smonkey.domain.user.exception.error.UserErrorCode
 import com.saehyun.smonkey.global.exception.GlobalException
 
+/**
+ * 유저가 존재하지 않을 경우의 [Exception]
+ *
+ * @throws UserNotFoundException
+ */
 object UserNotFoundException: GlobalException(UserErrorCode.USER_NOT_FOUND) {
     val EXCEPTION = UserNotFoundException
 }
