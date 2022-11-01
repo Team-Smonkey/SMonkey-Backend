1ec4b8a12963b0a982ed4dc54796e4fd3f6ea9da
limsaehyun <sh00710000@gmail.com>
2022-11-02 00:22:30 +0900

    style: (#7) clean up code

--- src/main/kotlin/com/saehyun/smonkey/domain/user/controller/UserController.kt
+++ src/main/kotlin/com/saehyun/smonkey/domain/user/controller/UserController.kt
@@ -1,27 +1,37 @@
 package com.saehyun.smonkey.domain.user.controller
 
+import com.saehyun.smonkey.domain.user.payload.request.SignInRequest
 import com.saehyun.smonkey.domain.user.payload.request.SignUpRequest
+import com.saehyun.smonkey.domain.user.service.SignInService
 import com.saehyun.smonkey.domain.user.service.SignUpService
 import com.saehyun.smonkey.global.payload.BaseResponse
 import com.saehyun.smonkey.global.security.jwt.payload.TokenResponse
 import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseStatus
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RequestMapping("/user")
 @RestController
 class UserController(
     private val signUpService: SignUpService,
+    private val signInService: SignInService,
 ) {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    fun signUpUser(
+    fun signUp(
         @RequestBody request: SignUpRequest,
     ): BaseResponse<TokenResponse> {
-        return signUpService.signUp(request)
+        return signUpService.signUp(
+            request = request,
+        )
+    }
+
+    @GetMapping
+    fun signIn(
+        @RequestBody request: SignInRequest,
+    ): BaseResponse<TokenResponse> {
+        return signInService.signIn(
+            request = request,
+        )
     }
 }
