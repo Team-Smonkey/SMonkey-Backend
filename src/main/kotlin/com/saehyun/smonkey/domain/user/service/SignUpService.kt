88edd717283483bee21a72802ccd2e1dd0c0f0a7
limsaehyun <sh00710000@gmail.com>
2022-11-02 00:21:12 +0900

    feat: (#5) setup RefreshToken

--- src/main/kotlin/com/saehyun/smonkey/domain/user/service/SignUpService.kt
+++ src/main/kotlin/com/saehyun/smonkey/domain/user/service/SignUpService.kt
@@ -1,13 +1,16 @@
 package com.saehyun.smonkey.domain.user.service
 
+import com.saehyun.smonkey.domain.user.entity.RefreshToken
 import com.saehyun.smonkey.domain.user.entity.User
 import com.saehyun.smonkey.domain.user.exception.UserAlreadyExistException
 import com.saehyun.smonkey.domain.user.facade.UserFacade
 import com.saehyun.smonkey.domain.user.payload.request.SignUpRequest
+import com.saehyun.smonkey.domain.user.repository.RefreshTokenRepository
 import com.saehyun.smonkey.domain.user.repository.UserRepository
 import com.saehyun.smonkey.global.payload.BaseResponse
 import com.saehyun.smonkey.global.security.jwt.JwtTokenProvider
 import com.saehyun.smonkey.global.security.jwt.payload.TokenResponse
+import com.saehyun.smonkey.global.security.properties.JwtProperties
 import org.springframework.security.crypto.password.PasswordEncoder
 import org.springframework.stereotype.Service
 
@@ -16,10 +19,13 @@ private const val SIGN_UP_MESSAGE = "sign up success"
 @Service
 class SignUpService(
     private val userRepository: UserRepository,
+    private val refreshTokenRepository: RefreshTokenRepository,
     private val jwtTokenProvider: JwtTokenProvider,
     private val passwordEncoder: PasswordEncoder,
     private val userFacade: UserFacade,
+    jwtProperties: JwtProperties,
 ) {
+    private val refreshExp: Long = jwtProperties.refreshTokenExp
 
     fun signUp(request: SignUpRequest): BaseResponse<TokenResponse> {
         if(userFacade.getUserExist(request.accountId)) throw UserAlreadyExistException
@@ -34,7 +40,17 @@ class SignUpService(
 
         userRepository.save(user)
 
-        val tokenResponse = jwtTokenProvider.getToken(user.accountId)
+        val tokenResponse = jwtTokenProvider.getToken(
+            accountId = user.accountId,
+        )
+
+        val refreshToken = RefreshToken(
+            accountId = request.accountId,
+            token = tokenResponse.refreshToken,
+            ttl = refreshExp,
+        )
+
+        refreshTokenRepository.save(refreshToken)
 
         return BaseResponse(
             status = 201,
