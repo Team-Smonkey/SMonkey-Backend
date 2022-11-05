c9caf95ae0a4686a275debc8d861f38de2ef6dc7
limsaehyun <sh00710000@gmail.com>
2022-11-05 11:10:58 +0900

    fix: (#11) SecurityConfig 개선

---
+++ src/main/kotlin/com/saehyun/smonkey/global/security/config/SecurityConfig.kt
@@ -0,0 +1,50 @@
+package com.saehyun.smonkey.global.security.config
+
+
+import com.fasterxml.jackson.databind.ObjectMapper
+import com.saehyun.smonkey.global.security.filter.FilterConfig
+import com.saehyun.smonkey.global.security.jwt.JwtTokenProvider
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+import org.springframework.http.HttpMethod
+import org.springframework.security.config.annotation.web.builders.HttpSecurity
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
+import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
+
+@Configuration
+@EnableWebSecurity
+class SecurityConfig(
+    private val jwtTokenProvider: JwtTokenProvider,
+    private val objectMapper: ObjectMapper,
+) {
+
+
+    @Bean
+    protected fun filterChain(http: HttpSecurity): SecurityFilterChain {
+        http
+            .cors().and()
+            .csrf().disable()
+            .formLogin().disable()
+
+            .authorizeRequests()
+            .antMatchers(HttpMethod.POST, "/smonkey").authenticated()
+            .antMatchers(HttpMethod.PATCH, "/smonkey").authenticated()
+            .antMatchers("/user").permitAll()
+            .anyRequest().denyAll()
+            .and()
+
+            .apply(
+                FilterConfig(
+                    jwtTokenProvider = jwtTokenProvider,
+                    objectMapper = objectMapper,
+                )
+            )
+
+        return http.build()
+    }
+
+    @Bean
+    fun passwordEncoder(): PasswordEncoder = BCryptPasswordEncoder()
+}
