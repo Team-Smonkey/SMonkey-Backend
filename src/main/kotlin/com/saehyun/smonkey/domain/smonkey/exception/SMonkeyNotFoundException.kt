3b8f06d6e8fa059219969e95b67fc6788c68bc60
limsaehyun <sh00710000@gmail.com>
2022-11-05 11:10:45 +0900

    init: (#11) SMonkeyNotFoundException

---
+++ src/main/kotlin/com/saehyun/smonkey/domain/smonkey/exception/SMonkeyNotFoundException.kt
@@ -0,0 +1,8 @@
+package com.saehyun.smonkey.domain.smonkey.exception
+
+import com.saehyun.smonkey.domain.smonkey.exception.error.SMonkeyErrorCode
+import com.saehyun.smonkey.global.exception.GlobalException
+
+object SMonkeyNotFoundException: GlobalException(SMonkeyErrorCode.SMONKEY_NOT_FOUND) {
+    val EXCEPTION = SMonkeyNotFoundException
+}
