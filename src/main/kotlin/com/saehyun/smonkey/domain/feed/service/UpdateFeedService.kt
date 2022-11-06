9c99043b368d8d2a08a71f7fdac7878f7eeec171
limsaehyun <sh00710000@gmail.com>
2022-11-06 16:41:04 +0900

    init: (#19) UpdateFeedService

---
+++ src/main/kotlin/com/saehyun/smonkey/domain/feed/service/UpdateFeedService.kt
@@ -0,0 +1,45 @@
+package com.saehyun.smonkey.domain.feed.service
+
+import com.saehyun.smonkey.domain.feed.exception.NotMyFeedException
+import com.saehyun.smonkey.domain.feed.facade.FeedFacade
+import com.saehyun.smonkey.domain.feed.payload.request.UpdateFeedRequest
+import com.saehyun.smonkey.domain.user.facade.UserFacade
+import com.saehyun.smonkey.global.payload.BaseResponse
+import org.springframework.stereotype.Service
+import org.springframework.transaction.annotation.Transactional
+
+@Service
+class UpdateFeedService(
+    private val feedFacade: FeedFacade,
+    private val userFacade: UserFacade
+) {
+
+    @Transactional
+    fun updateFeed(
+        feedId: Long,
+        request: UpdateFeedRequest,
+    ): BaseResponse<Unit> {
+        val feed = feedFacade.getFeedById(feedId)
+        val user = userFacade.getCurrentUser()
+
+        if (feed.user != user) {
+            throw NotMyFeedException
+        }
+
+        feed.update(
+            title = feed.title,
+            content = feed.content,
+            category = feed.category,
+        )
+
+        return BaseResponse(
+            status = 200,
+            message = FEED_UPDATE_SUCCESS_MESSAGE,
+            content = null,
+        )
+    }
+
+    companion object {
+        const val FEED_UPDATE_SUCCESS_MESSAGE = "success to update feed"
+    }
+}
