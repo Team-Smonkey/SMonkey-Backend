42dafe077cc3ebd0c1390165971230cb4c34c281
limsaehyun <sh00710000@gmail.com>
2022-11-06 16:07:38 +0900

    feat: (#15) StrToFeedType Mapper

---
+++ src/main/kotlin/com/saehyun/smonkey/domain/feed/mapper/StrToFeedType.kt
@@ -0,0 +1,13 @@
+package com.saehyun.smonkey.domain.feed.mapper
+
+import com.saehyun.smonkey.domain.feed.enum.FeedType
+import com.saehyun.smonkey.domain.feed.exception.FeedTypeNotExistException
+
+internal fun String.toFeedType(): FeedType {
+
+    for(feedType in FeedType.values()) {
+        if(feedType.toString() == this) return feedType
+    }
+
+    throw FeedTypeNotExistException
+}
