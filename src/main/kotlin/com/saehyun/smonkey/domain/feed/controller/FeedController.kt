f16792d98e64d5933adb58d10dc17936a2da8880
limsaehyun <sh00710000@gmail.com>
2022-11-06 16:41:46 +0900

    feat: (#19) controller updateFeed

--- src/main/kotlin/com/saehyun/smonkey/domain/feed/controller/FeedController.kt
+++ src/main/kotlin/com/saehyun/smonkey/domain/feed/controller/FeedController.kt
@@ -1,27 +1,37 @@
 package com.saehyun.smonkey.domain.feed.controller
 
-import com.saehyun.smonkey.domain.feed.payload.request.FeedRequest
+import com.saehyun.smonkey.domain.feed.payload.request.SaveFeedRequest
+import com.saehyun.smonkey.domain.feed.payload.request.UpdateFeedRequest
 import com.saehyun.smonkey.domain.feed.service.SaveFeedService
+import com.saehyun.smonkey.domain.feed.service.UpdateFeedService
 import com.saehyun.smonkey.global.payload.BaseResponse
 import org.springframework.http.HttpStatus
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.ResponseStatus
-import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.bind.annotation.*
 
 @RequestMapping("/feed")
 @RestController
 class FeedController(
     private val saveFeedService: SaveFeedService,
+    private val updateFeedService: UpdateFeedService,
 ) {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
     fun saveFeed(
-        @RequestBody request: FeedRequest,
+        @RequestBody request: SaveFeedRequest,
     ): BaseResponse<Unit> {
         return saveFeedService.saveFeed(
+            request = request,
+        )
+    }
+
+    @PatchMapping("/{feed-id}")
+    fun updateFeed(
+        @PathVariable("feed-id") feedId: Long,
+        @RequestBody request: UpdateFeedRequest,
+    ): BaseResponse<Unit> {
+        return updateFeedService.updateFeed(
+            feedId = feedId,
             request = request,
         )
     }
