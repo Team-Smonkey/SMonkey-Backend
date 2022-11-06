15014185c8bdd41dda7143fc183f879525b1be24
limsaehyun <sh00710000@gmail.com>
2022-11-06 16:07:50 +0900

    init: (#15) JpaConfig

---
+++ src/main/kotlin/com/saehyun/smonkey/global/jpa/JpaConfig.kt
@@ -0,0 +1,8 @@
+package com.saehyun.smonkey.global.jpa
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+
+@EnableJpaAuditing
+@Configuration
+class JpaConfig
