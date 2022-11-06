7b3292ccef18c9e388ccbed97a8882a3e990e5dc
limsaehyun <sh00710000@gmail.com>
2022-11-06 16:06:54 +0900

    init: (#15) FeedTypeNotExistException

---
+++ src/main/kotlin/com/saehyun/smonkey/domain/feed/exception/FeedTypeNotExistException.kt
@@ -0,0 +1,8 @@
+package com.saehyun.smonkey.domain.feed.exception
+
+import com.saehyun.smonkey.domain.feed.exception.error.FeedErrorCode
+import com.saehyun.smonkey.global.exception.GlobalException
+
+object FeedTypeNotExistException: GlobalException(FeedErrorCode.FEED_TYPE_NOT_EXIST) {
+    val EXCEPTION = FeedTypeNotExistException
+}
