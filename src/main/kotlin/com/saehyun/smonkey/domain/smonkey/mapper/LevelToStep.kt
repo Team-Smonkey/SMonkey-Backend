763b6e27ba3d1b3cd2f427ad7af9aa7727f5b5e2
limsaehyun <sh00710000@gmail.com>
2022-11-06 14:40:42 +0900

    feat: (#13) mapping LevelToStep

---
+++ src/main/kotlin/com/saehyun/smonkey/domain/smonkey/mapper/LevelToStep.kt
@@ -0,0 +1,12 @@
+package com.saehyun.smonkey.domain.smonkey.mapper
+
+import com.saehyun.smonkey.domain.smonkey.exception.SmokingStepNotFoundException
+import com.saehyun.smonkey.domain.smonkey.utils.NoSmokingSteps
+
+internal fun Int.toStep(): Int {
+    NoSmokingSteps.forEach { step ->
+        if(this >= step.minLevel) return step.step
+    }
+
+    throw SmokingStepNotFoundException.EXCEPTION
+}
