6ed04642ae1a6719775a7cfc979ce4bd237bae88
limsaehyun <sh00710000@gmail.com>
2022-11-08 11:55:10 +0900

    feat: (#25) LikeRepository

---
+++ src/main/kotlin/com/saehyun/smonkey/domain/like/repository/LikeRepository.kt
@@ -0,0 +1,15 @@
+package com.saehyun.smonkey.domain.like.repository
+
+import com.saehyun.smonkey.domain.like.entity.FeedLike
+import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface LikeRepository: CrudRepository<FeedLike, Long> {
+
+    fun findByFeedId(feedId: Long): List<FeedLike>?
+
+    fun findByUserId(userId: Long): List<FeedLike>
+
+    fun findByFeedIdAndUserId(feedId: Long, userId: Long): FeedLike?
+}
