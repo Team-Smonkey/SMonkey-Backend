d401d44626dfdff7f078081df8452c7ebb078928
limsaehyun <sh00710000@gmail.com>
2022-11-08 11:53:31 +0900

    init: (#25) FeedLike Entity

---
+++ src/main/kotlin/com/saehyun/smonkey/domain/like/entity/FeedLike.kt
@@ -0,0 +1,22 @@
+package com.saehyun.smonkey.domain.like.entity
+
+import com.saehyun.smonkey.domain.feed.entity.Feed
+import com.saehyun.smonkey.domain.user.entity.User
+import javax.persistence.*
+
+@Entity
+@Table(name = "post_likes")
+class FeedLike(
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Long = 0,
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
+    var user: User,
+
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "feed_id")
+    var feed: Feed,
+)
