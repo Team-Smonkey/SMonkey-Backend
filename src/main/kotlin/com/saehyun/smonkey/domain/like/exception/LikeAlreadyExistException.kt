ce307b8e46e5a9e7fb3073d3f5d90831484a9a4f
limsaehyun <sh00710000@gmail.com>
2022-11-08 11:54:26 +0900

    init: (#25) LikeAlreadyExistException

---
+++ src/main/kotlin/com/saehyun/smonkey/domain/like/exception/LikeAlreadyExistException.kt
@@ -0,0 +1,8 @@
+package com.saehyun.smonkey.domain.like.exception
+
+import com.saehyun.smonkey.domain.like.exception.error.LikeErrorCode
+import com.saehyun.smonkey.global.exception.GlobalException
+
+object LikeAlreadyExistException: GlobalException(LikeErrorCode.LIKE_ALREADY_EXIST) {
+    val EXCEPTION = LikeAlreadyExistException
+}
