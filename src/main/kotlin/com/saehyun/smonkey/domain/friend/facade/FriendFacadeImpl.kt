3c17048662582f854f3d1dfee11b2049adbb6b7c
limsaehyun <sh00710000@gmail.com>
2022-11-14 18:23:29 +0900

    feat: (#34) get friend facade

--- src/main/kotlin/com/saehyun/smonkey/domain/friend/facade/FriendFacadeImpl.kt
+++ src/main/kotlin/com/saehyun/smonkey/domain/friend/facade/FriendFacadeImpl.kt
@@ -3,6 +3,8 @@ package com.saehyun.smonkey.domain.friend.facade
 import com.saehyun.smonkey.domain.friend.entity.Friend
 import com.saehyun.smonkey.domain.friend.exception.FriendNotFoundException
 import com.saehyun.smonkey.domain.friend.repository.FriendRepository
+import com.saehyun.smonkey.domain.friend.utils.FriendStatus
+import com.saehyun.smonkey.domain.user.entity.User
 import org.springframework.stereotype.Component
 
 @Component
@@ -34,6 +36,28 @@ class FriendFacadeImpl(
             senderId = senderId,
             receiverId = receiverId,
         )
+    }
+
+    override fun getFriendList(userId: Long): List<User> {
+
+        val statuses = listOf(
+            FriendStatus.ACCEPTED,
+            FriendStatus.ACCEPTED_REVERSE,
+        )
+
+        val senderId: List<User> = friendRepository.getAllBySenderIdAndStatusIn(
+            senderId = userId,
+            statuses = statuses,
+        ).map { it.receiver }
+
+        val receiverId: List<User> = friendRepository.getAllByReceiverIdAndStatusIn(
+            receiverId = userId,
+            statuses = statuses,
+        ).map { it.sender }
+
+        val friendList = senderId.union(receiverId)
+
+        return friendList.toList()
     }
 
 }
