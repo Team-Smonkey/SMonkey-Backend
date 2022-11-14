4f82bd08b4851fb52f66c7aa17e5406c02aab36e
limsaehyun <sh00710000@gmail.com>
2022-11-14 13:08:54 +0900

    feat: (#32) friend repository

---
+++ src/main/kotlin/com/saehyun/smonkey/domain/friend/repository/FriendRepository.kt
@@ -0,0 +1,28 @@
+package com.saehyun.smonkey.domain.friend.repository
+
+import com.saehyun.smonkey.domain.friend.entity.Friend
+import com.saehyun.smonkey.domain.friend.utils.FriendStatus
+import org.springframework.data.repository.CrudRepository
+
+interface FriendRepository: CrudRepository<Friend, Long> {
+
+    fun getAllBySenderIdAndStatusIn(
+        senderId: Long,
+        statuses: List<FriendStatus>,
+    ): List<Friend>
+
+    fun getAllByReceiverIdAndStatusIn(
+        receiverId: Long,
+        status: List<FriendStatus>,
+    ): List<Friend>
+
+    fun existsBySenderIdAndReceiverId(
+        senderId: Long,
+        receiverId: Long,
+    ): Boolean
+
+    fun getBySenderIdAndReceiverId(
+        senderId: Long,
+        receiverId: Long,
+    ): Friend?
+}
