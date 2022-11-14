e7c8f99cd3f0a2cd6fd1dfeace4b53625c972dbd
limsaehyun <sh00710000@gmail.com>
2022-11-14 19:54:46 +0900

    feat: (#39) SmokingFacade

---
+++ src/main/kotlin/com/saehyun/smonkey/domain/smoking/facade/SmokingFacade.kt
@@ -0,0 +1,15 @@
+package com.saehyun.smonkey.domain.smoking.facade
+
+import com.saehyun.smonkey.domain.smoking.entity.Smoking
+
+
+interface SmokingFacade {
+
+    fun getSmokingByUserId(
+        userId: Long,
+    ): Smoking
+
+    fun saveSmoking(
+        smoking: Smoking,
+    )
+}
