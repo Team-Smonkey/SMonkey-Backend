5572191a89ebcb655784e9d671d3cea2fb31c6da
limsaehyun <sh00710000@gmail.com>
2022-11-14 13:09:29 +0900

    init: (#32) FriendAlreadyExistException

---
+++ src/main/kotlin/com/saehyun/smonkey/domain/friend/exception/FriendAlreadyExistException.kt
@@ -0,0 +1,8 @@
+package com.saehyun.smonkey.domain.friend.exception
+
+import com.saehyun.smonkey.domain.friend.exception.error.FriendErrorCode
+import com.saehyun.smonkey.global.exception.GlobalException
+
+object FriendAlreadyExistException: GlobalException(FriendErrorCode.FRIEND_ALREADY_EXIST) {
+    val EXCEPTION = FriendAlreadyExistException
+}
