2ab694867009d8ad97cf621bdf7445e00e88ca7f
limsaehyun <sh00710000@gmail.com>
2022-11-14 13:10:05 +0900

    feat: (#32) request friend service

---
+++ src/main/kotlin/com/saehyun/smonkey/domain/friend/service/RequestFriendService.kt
@@ -0,0 +1,51 @@
+package com.saehyun.smonkey.domain.friend.service
+
+import com.saehyun.smonkey.domain.friend.entity.Friend
+import com.saehyun.smonkey.domain.friend.exception.FriendAlreadyExistException
+import com.saehyun.smonkey.domain.friend.facade.FriendFacade
+import com.saehyun.smonkey.domain.friend.payload.request.FriendRequest
+import com.saehyun.smonkey.domain.friend.utils.FriendStatus
+import com.saehyun.smonkey.domain.user.facade.UserFacade
+import com.saehyun.smonkey.global.payload.BaseResponse
+import org.springframework.stereotype.Service
+
+@Service
+class RequestFriendService(
+    private val friendFacade: FriendFacade,
+    private val userFacade: UserFacade,
+) {
+
+    fun requestFriend(
+        request: FriendRequest,
+    ): BaseResponse<Unit> {
+        val sender = userFacade.getCurrentUser()
+        val receiver = userFacade.getById(
+            id = request.receiverId,
+        )
+
+        friendFacade.existsBySenderIdAndReceiverId(
+            senderId = sender.id,
+            receiverId = receiver.id
+        ).also {
+            if(it) throw FriendAlreadyExistException
+        }
+
+        friendFacade.createFriend(
+            Friend(
+                sender = sender,
+                receiver = receiver,
+                status = FriendStatus.PENDING,
+            )
+        )
+
+        return BaseResponse(
+            status = 201,
+            message = REQUEST_FRIEND_SUCCESS_MESSAGE,
+            content = null,
+        )
+    }
+
+    companion object {
+        const val REQUEST_FRIEND_SUCCESS_MESSAGE = "success to reqeust friend"
+    }
+}
