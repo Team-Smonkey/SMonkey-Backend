1a3f61a96425610e77647f57ef47df475baab06f
limsaehyun <sh00710000@gmail.com>
2022-11-16 21:52:39 +0900

    fix: edit smonkey color

--- src/main/kotlin/com/saehyun/smonkey/domain/smonkey/utils/Constans.kt
+++ src/main/kotlin/com/saehyun/smonkey/domain/smonkey/utils/Constans.kt
@@ -3,6 +3,26 @@ package com.saehyun.smonkey.domain.smonkey.utils
 internal const val DefaultPoint: Int = 0
 
 internal val colorList: List<String> = listOf(
-    "WHITE",
-    "BLACK",
+    "SeaGreen",
+    "AppleGreen",
+    "YellowGreen",
+    "LightYellow",
+    "LightOrange",
+    "LightRed",
+    "Cerise",
+    "Magenta",
+    "IndianRed",
+    "LightCoral",
+    "Bisqure",
+    "Blossom",
+    "Pink",
+    "DarkPink",
+    "Plum",
+    "SkyBlue",
+    "LightAqua",
+    "LightBlue",
+    "Violet",
+    "Orchid",
+    "Purple",
+    "Navy",
 )
